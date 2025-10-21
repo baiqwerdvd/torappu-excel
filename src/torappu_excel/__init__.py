a351f9351b5964e6455d0de92ff73012ef72fa1e
baiqwerdvd <158065462+baiqwerdvd@users.noreply.github.com>
2025-10-21 17:47:32 +0800

    release: support 25-10-20-08-03-10_8f5563

--- src/torappu_excel/__init__.py
+++ src/torappu_excel/__init__.py
@@ -1,8 +1,8 @@
-__version__ = "2.6.66"
+__version__ = "2.6.67"
 
 client_version = "2.6.61"
-data_version = "25-10-13-16-21-56_229ee9"
-major_version = 396
+data_version = "25-10-20-08-03-10_8f5563"
+major_version = 398
 
 from .constants import ExcelTableManager as ExcelTableManager  # noqa: E402
 from .torappu.player_data_model import PlayerDataModel as PlayerDataModel  # noqa: E402
