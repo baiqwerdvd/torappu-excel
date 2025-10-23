30b692a7860656a446731e1d023a5876138d37f9
baiqwerdvd <158065462+baiqwerdvd@users.noreply.github.com>
2025-10-23 14:56:38 +0800

    fix: 更新版本号和依赖项，修复测试工作流中的Python设置

--- src/torappu_excel/__init__.py
+++ src/torappu_excel/__init__.py
@@ -1,8 +1,8 @@
-__version__ = "2.6.67"
+__version__ = "2.6.68"
 
 client_version = "2.6.61"
-data_version = "25-10-20-08-03-10_8f5563"
-major_version = 398
+data_version = "25-10-22-11-51-22_eda48a"
+major_version = 399
 
 from .constants import ExcelTableManager as ExcelTableManager  # noqa: E402
 from .torappu.player_data_model import PlayerDataModel as PlayerDataModel  # noqa: E402
