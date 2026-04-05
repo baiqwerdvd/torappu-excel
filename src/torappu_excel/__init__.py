1b15672d14f8ea65b9096ab5a938a2176f134f73
baiqwerdvd <158065462+baiqwerdvd@users.noreply.github.com>
2026-04-05 20:49:10 +0800

    fix: use Win resversion

--- src/torappu_excel/__init__.py
+++ src/torappu_excel/__init__.py
@@ -1,7 +1,7 @@
-__version__ = "2.7.12"
+__version__ = "2.7.13"
 
 client_version = "2.7.11"
-data_version = "26-03-31-05-42-28_7d7f67"
+data_version = "26-03-31-05-51-38_1cc8a5"
 major_version = 425
 network_version = "5"
 
